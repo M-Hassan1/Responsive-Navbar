bb7fb5a6dd1346f304fb7c62ec9b17677b8c465a
Muhammad Hassan <128030276+M-Hassan1@users.noreply.github.com>
2023-03-19 12:40:19 +0500

    Update navigationbar.js

--- navigationbar.js
+++ navigationbar.js
@@ -20,44 +20,4 @@ function work(){
 
   }
   work()
-
 }
-
-
-
-
-
-
-
-
-
-// let imgs = document.getElementById('ph')
-// let inputspalce = document.getElementById('file-up')
-// inputspalce.onchange = function(){
-//   imgs.src = URL.createObjectURL(inputspalce.files[0])
-// }
-// // Convert image to data URL
-// var canvas = document.createElement("canvas");
-// var img = document.getElementById("ph");
-// canvas.width = img.width;
-// canvas.height = img.height;
-// var ctx = canvas.getContext("2d");
-// ctx.drawImage(img, 0, 0);
-// var dataURL = canvas.toDataURL();
-
-// // Store image and input data in localStorage
-// var inputElement = document.getElementById("file-up");
-// var inputData = inputElement.value;
-// localStorage.setItem("ph", dataURL);
-// localStorage.setItem("file-up", inputData);
-
-// // Retrieve image and input data from localStorage
-// var storedImage = new Image();
-// var storedImageDataURL = localStorage.getItem("ph");
-// storedImage.src = storedImageDataURL;
-// document.body.appendChild(storedImage);
-
-// var storedInputData = localStorage.getItem("file-up");
-// if (storedInputData !== null) {
-//   inputElement.value = storedInputData;
-// }
